1f43aaf5f64ba8cfb6261eaa9142053092d13f39
nim4karimi <nima.karimi@live.com>
2018-03-08 15:06:39 +0330

    install first app

--- uromkala/urls.py
+++ uromkala/urls.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+
 """uromkala URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +22,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
